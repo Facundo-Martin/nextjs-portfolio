cc666ec0bc63030e8fb5d0b0c7b6f180258c89e7
Facundo Martin <facundomartin98@gmail.com>
2025-04-24 17:27:59 -0300

    fix: /article copy and metadata

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -37,7 +37,7 @@ function Article({ article }: { article: ArticleWithSlug }) {
 export const metadata: Metadata = {
   title: 'Articles',
   description:
-    'All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.',
+    "A journal of my adventures in building software for startups. These posts capture the challenges, solutions, and occasional 'aha!' moments from my engineering journey.",
 }
 
 export default async function ArticlesIndex() {
@@ -45,8 +45,8 @@ export default async function ArticlesIndex() {
 
   return (
     <SimpleLayout
-      title="Writing on software design, company building, and the aerospace industry."
-      intro="All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order."
+      title="Writing on web development, architectural patterns, and startup engineering"
+      intro="A journal of my adventures in building software for startups. These posts capture the challenges, solutions, and occasional 'aha!' moments from my engineering journey."
     >
       <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
         <div className="flex max-w-3xl flex-col space-y-16">
