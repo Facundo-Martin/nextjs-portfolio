1ea651aa1089c4b45d90fde542826b017d98b2ad
Facundo Martin <facundomartin98@gmail.com>
2025-04-29 18:09:12 -0300

    fix: heading style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -287,7 +287,8 @@ export default async function Home() {
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
-            Code craftsman, engineer, architecture enthusiast.
+            Code craftsman. <br /> Software engineer. <br /> Architecture
+            enthusiast.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
             I&apos;m Facundo. I build software with an emphasis on clean
